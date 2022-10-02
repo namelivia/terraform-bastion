96d3da5995eabf937e516bb9dcd4ce4ea9e7a0ed
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-10-02 12:36:22 +0200

    Update parameters

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_lightsail_key_pair" "deployer" {
 
 resource "aws_lightsail_instance" "lightsail_instance" {
   name = "${var.instance_name}"
-  availability_zone = "eu-west-3c"
+  availability_zone = "eu-west-3a"
   key_pair_name = "${var.instance_name}-key"
-  blueprint_id = "ubuntu_18_04"
-  bundle_id = "small_2_0"
+  blueprint_id = "ubuntu_20_04"
+  bundle_id = "micro_2_0"
 }
